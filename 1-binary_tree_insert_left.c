9bcce90de06befa9eb78bb24f3ccdc91aee0453d
Matias <3959@holbertonschool.com>
2022-03-22 12:03:22 -0700

    fix codee, add error value NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	temp = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
+	if (!value)
+		return (NULL);
+
 	if (temp == NULL)
 		return (NULL);
 
